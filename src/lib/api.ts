7c20a48143ffe3414042a6a8708fd58434f5caf9
setraniainabruno <brunoharison18@yahoo.com>
2025-05-09 09:07:36 +0300

    change lien endpoint

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8090/api'
+  // baseURL: 'http://localhost:8090/api'
+  baseURL: 'https://gestion-chambre-api.onrender.com/api/'
 });
 
 export default api;
